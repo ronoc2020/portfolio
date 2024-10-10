8fb21f78cc2624074d61d6fe0fc7e992623bc917
R.M. Orlowski <69809290+ronoc2020@users.noreply.github.com>
2024-10-10 22:26:17 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Important for generating a static site
   eslint: {
     ignoreDuringBuilds: true, // Skip ESLint errors during builds
   },
   images: { unoptimized: true }, // Disable Next.js image optimization
-  // If you later need rewrites or custom handling, you can add it here
+  distDir: 'build', // Specify a custom output directory
+  // Additional configurations can go here
 };
 
 module.exports = nextConfig;
